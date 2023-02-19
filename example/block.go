99f487b8fc9545c786836703a2c9e7b116145221
William <firewings.r@gmail.com>
2023-02-19 13:29:12 +0800

    part three use blotdb

--- example/block.go
+++ example/block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -13,18 +16,46 @@ type Block struct {
 	Nonce         int
 }
 
+// Serialize serializes the block
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
 // NewBlock creates and returns Block
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{prevBlockHash, time.Now().Unix(), []byte{}, []byte(data), 0}
 
 	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
+
 	block.Hash = hash[:]
 	block.Nonce = nonce
+
 	return block
 }
 
 // NewGenesisBlock creates and returns genesis Block
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+// DeserializeBlock deserializes a block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
--- example/main.go
+++ example/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/big"
-	"strconv"
 )
 
 func main() {
@@ -14,15 +13,7 @@ func main() {
 	bc.AddBlock("Send 1 BTC To Densey")
 	bc.AddBlock("Send 2 more BTC To Densey")
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("Nonce: %x\n", block.Nonce)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	printChain(bc.Iterator())
 }
 
 func main2() {
